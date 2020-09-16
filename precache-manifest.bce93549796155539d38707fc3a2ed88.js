f892e86bbb8fa5f2a8e10eb15261c88756b97eb2
AppVeyor <ci@appveyor.com>
2020-09-16 18:50:05 +0000

    Updates

---
+++ precache-manifest.bce93549796155539d38707fc3a2ed88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada8ab815b0535b9e16e9043297a9aa4",
+    "url": "/React-1.2/index.html"
+  },
+  {
+    "revision": "820c13555d5412d76ea4",
+    "url": "/React-1.2/static/css/main.8e1e6f3b.chunk.css"
+  },
+  {
+    "revision": "1a2dc1e2c1898389f8f3",
+    "url": "/React-1.2/static/js/2.adb80773.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-1.2/static/js/2.adb80773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820c13555d5412d76ea4",
+    "url": "/React-1.2/static/js/main.f81bc301.chunk.js"
+  },
+  {
+    "revision": "f08d4487ff8af15595b3",
+    "url": "/React-1.2/static/js/runtime-main.8a3aafb5.js"
+  },
+  {
+    "revision": "9c63a2c92146aa97904b9148bc055ce3",
+    "url": "/React-1.2/static/media/item-black.9c63a2c9.jpg"
+  },
+  {
+    "revision": "8bcbdffead0d5ba6700c1551189af1b8",
+    "url": "/React-1.2/static/media/item-blue.8bcbdffe.jpg"
+  }
+]);
