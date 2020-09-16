3956a9492b7ac1f8ae256514ff8ae40069e8ae76
MihailOkatev <mokatev@yandex.ru>
2020-09-16 21:48:38 +0300

    class updated

--- src/components/ShopItemFunc.js
+++ src/components/ShopItemFunc.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types';
 import ItemModel from "../models/ItemModel";
 class ShopItemFunc extends React.Component {
+    // eslint-disable-next-line no-useless-constructor
     constructor(props) {
         super(props);
     }
